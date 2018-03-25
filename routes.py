56aebd74dad7d4f6e9a0e1ec807fd289a2459a9e
SteveDengZishi <zd475@nyu.edu>
2018-03-25 11:25:39 -0400

    secure forms

--- routes.py
+++ routes.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
+from forms import SignupForm
 
 app = Flask(__name__)
 
+app.secret_key = "development-key"
+
 @app.route("/")
 def login():
-	return render_template("login.html")
+	form = SignupForm();
+	return render_template("login.html", form=form)
 
 if __name__ == "__main__":
 	app.run(debug=True)
