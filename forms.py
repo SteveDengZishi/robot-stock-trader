63037011e221d2a6b61734c64d2fea46b490a9dc
SteveDengZishi <zd475@nyu.edu>
2018-03-25 12:33:05 -0400

    finishing wtforms

--- forms.py
+++ forms.py
@@ -2,11 +2,11 @@ from flask_wtf import Form
 from wtforms import StringField, PasswordField, SubmitField, SelectField, IntegerField
 
 class SignupForm(Form):
-	risks = ['Volatile, High risk with high reward','Moderate, Move at a relatively steady pace','Safe, Move little and minimize risk']
+	risks = [('Volatile', 'Volatile, High risk with high reward'),('Moderate', 'Moderate, Move at a relatively steady pace'),('Safe', 'Safe, Move little and minimize risk')]
 	username = StringField('Username')
 	email = StringField('Email')
-	password = PasswordField('Password')
-	password1 = PasswordField('Re-type Password')
-	investment = IntegerField('Investment Amount in USD')
+	password = PasswordField('Enter your password')
+	password1 = PasswordField('Re-type your password')
+	investment = IntegerField('Enter your investment in USD')
 	riskLevel = SelectField('Select the level of risk you can afford', choices=risks)
 	submit = SubmitField('Get Started')
--- routes.py
+++ routes.py
@@ -1,14 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from forms import SignupForm
 
 app = Flask(__name__)
 
 app.secret_key = "development-key"
 
-@app.route("/")
+@app.route("/", methods=['GET','POST'])
 def login():
 	form = SignupForm();
-	return render_template("login.html", form=form)
+
+	if request.method == 'POST':
+		return "success!"
+		
+	else:
+		return render_template("login.html", form=form)
 
 if __name__ == "__main__":
 	app.run(debug=True)
