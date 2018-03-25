659406637a8cd981e1da85ed197450d0ab5aca90
SteveDengZishi <zd475@nyu.edu>
2018-03-25 14:15:28 -0400

    Email validation

--- forms.py
+++ forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, SelectField, IntegerField
-from wtforms.validators import Length, DataRequired, NumberRange, EqualTo
+from wtforms.validators import Length, DataRequired, NumberRange, EqualTo, Email
 
 class SignupForm(FlaskForm):
 	risks = [('Volatile', 'Volatile, High risk with high reward'),('Moderate', 'Moderate, Move at a relatively steady pace'),('Safe', 'Safe, Move little and minimize risk')]
 	username = StringField('Username', validators=[DataRequired(), Length(min=5, max=15)])
-	email = StringField('Email', validators=[DataRequired()])
+	email = StringField('Email', validators=[DataRequired(), Email("Please enter a vaild email address")])
 	password = PasswordField('Enter your password', validators=[DataRequired(), Length(min=6)])
 	password1 = PasswordField('Re-type your password', validators=[DataRequired(), Length(min=6), EqualTo(fieldname='password', message='Passwords do not match')])
 	investment = IntegerField('Enter your investment in USD', validators=[DataRequired(), NumberRange(min=100)])
