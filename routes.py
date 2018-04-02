c10ea9f71a6906b98da6f73cb34bd1748700747b
Rohan Reddy <rr2892@nyu.edu>
2018-04-02 12:09:27 -0400

    test of zipline connect

--- routes.py
+++ routes.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 from flask_sqlalchemy import SQLAlchemy
 from forms import SignupForm
+import pandas as pd
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+import numpy as np
+import StringIO
+import zipline
+import datetime
+from zipline.api import order, record, symbol
 
 app = Flask(__name__)
 
-app.secret_key = "development-key"
+app.secret_key = "development-ksey"
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://skqtsiamfhxtft:32d113d4b722c701dae458160b179e9c3f06cfab6fd5ccd9e31932afcba018b4@ec2-23-21-121-220.compute-1.amazonaws.com:5432/d14f5uhqoad2g'
 
@@ -31,6 +40,28 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
+def initialize(context):
+    pass
+
+def handle_data(context, data):
+    order(symbol('AAPL'), 10)
+    record(AAPL=data.current(symbol('AAPL'), 'price'))
+
+def make_fig(perf):
+    fig = Figure()
+    ax1 = fig.add_subplot(111)
+    perf.portfolio_value.plot(ax=ax1)
+    ax1.set_ylabel('portfolio value')
+    # ax2 = plt.subplot(112, sharex=ax1)
+    # perf.AAPL.plot(ax=ax2)
+    # ax2.set_ylabel('AAPL stock price')
+    canvas = FigureCanvas(fig)
+    png_output = StringIO.StringIO()
+    canvas.print_png(png_output)
+    response = make_response(png_output.getvalue())
+    response.headers['Content-Type'] = 'image/png'
+    return response
+
 @app.route("/", methods = ['GET', 'POST'])
 def login():
 	form = SignupForm();
@@ -40,6 +71,7 @@ def login():
 			return render_template("login.html", form=form)
 		else:
 			risk_level = form.riskLevel.data
+            capex = form.investment.data
 			if(risk_level == 'Volatile'):
 				risk_level_int = 0
 			if(risk_level == 'Moderate'):
@@ -49,7 +81,11 @@ def login():
 			newUser = User(form.username.data, form.email.data, form.password.data, form.investment.data, risk_level_int)
 			db.session.add(newUser)
 			db.session.commit()
-			return "success!"
+            start = datetime.date(2015, 1, 1)
+            end = datetime.date(2017, 1, 1)
+            perf = zipline.run_algorithm(start, end, initialize, capex, handle_data)
+            myplot = make_fig(perf)
+			return render_template("portfolio.html", myplot=myplot)
 
 	else:
 		return render_template("login.html", form=form)
