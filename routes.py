ec287e32b0e47942824af942e6cd0e0b0b2a5ec0
Rohan Reddy <rr2892@nyu.edu>
2018-04-12 13:15:17 -0400

    plotly testing

--- routes.py
+++ routes.py
@@ -80,18 +80,18 @@ def handle_data(context, data):
     record(AAPL=data.current(symbol('AAPL'), 'price'))
 
 def setup_zipline():
-    capital = 1000000
-    zp = Zipliner.getInstance()
-    start = pd.to_datetime('2015-01-01').tz_localize('US/Eastern')
-    end = pd.to_datetime('2017-01-01').tz_localize('US/Eastern')
-    df = zp.run(start, end, capital)
+    # capital = 1000000
+    # zp = Zipliner.getInstance()
+    # start = pd.to_datetime('2015-01-01').tz_localize('US/Eastern')
+    # end = pd.to_datetime('2017-01-01').tz_localize('US/Eastern')
+    # df = zp.run(start, end, capital)
     # data = [go.Scatter(x=df.iloc[:, 0], y=df['portfolio_value'])]
     # str = off.plot({
     #     "data": data,
     #     "layout": go.Layout(title="Portfolio Performance")
     # })
     content = off.plot(
-        data=[
+        [
             go.Scatter(x=[1, 2, 3, 4], y=[4, 1, 3, 7])
         ],
         # layout=go.Layout(
