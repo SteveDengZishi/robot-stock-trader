3cf155a75fb53e448e628e7a37451bb89e2460d3
Rohan Reddy <rr2892@nyu.edu>
2018-04-17 12:48:35 -0400

    new dual moving avg algo

--- routes.py
+++ routes.py
@@ -7,7 +7,8 @@ import numpy as np
 import pandas as pd
 import zipline
 from datetime import datetime
-from zipline.api import order, record, symbol
+from zipline.api import order_target, record, symbol
+from zipline.finance import commission, slippage
 import plotly.plotly as py
 import plotly.graph_objs as go
 import plotly.offline as off
@@ -80,11 +81,41 @@ def portfolio():
 
 
 def initialize(context):
-    pass
+    context.sym = symbol('AAPL')
+    context.i = 0
+
+    # Explicitly set the commission/slippage to the "old" value until we can
+    # rebuild example data.
+    # github.com/quantopian/zipline/blob/master/tests/resources/
+    # rebuild_example_data#L105
+    context.set_commission(commission.PerShare(cost=.0075, min_trade_cost=1.0))
+    context.set_slippage(slippage.VolumeShareSlippage())
+
 
 def handle_data(context, data):
-    order(symbol('AAPL'), 10)
-    record(AAPL=data.current(symbol('AAPL'), 'price'))
+    # Skip first 300 days to get full windows
+    context.i += 1
+    if context.i < 300:
+        return
+
+    # Compute averages
+    # history() has to be called with the same params
+    # from above and returns a pandas dataframe.
+    short_mavg = data.history(context.sym, 'price', 100, '1d').mean()
+    long_mavg = data.history(context.sym, 'price', 300, '1d').mean()
+
+    # Trading logic
+    if short_mavg > long_mavg:
+        # order_target orders as many shares as needed to
+        # achieve the desired number of shares.
+        order_target(context.sym, 100)
+    elif short_mavg < long_mavg:
+        order_target(context.sym, 0)
+
+    # Save values for later inspection
+    record(AAPL=data.current(context.sym, "price"),
+           short_mavg=short_mavg,
+           long_mavg=long_mavg)
 
 def setup_zipline():
     investment = session['investment'][1:-3]
